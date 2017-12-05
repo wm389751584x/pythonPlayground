9e2afb13e8ad66013c2a0b1861cdde058c01f7f4
Jason Wang <wangmenghw@gmail.com>
2017-12-04 22:04:57 -0600

    simple class in python

---
+++ Car_class.py
@@ -0,0 +1,25 @@
+class Car():
+    """A simple attempt to represent a car"""
+
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car"""
+        self.make = make
+        self.model = model
+        self.year = year
+        """Default Value for an attribute"""
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+
+    def read_odometer(self):
+        """Print a statement showing the car's mildage."""
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+
+
+my_new_car = Car('bmw', 'm3', 2018)
+print(my_new_car.get_descriptive_name())
+my_new_car.odometer_reading = 1000
+my_new_car.read_odometer()
--- dog_class.py
+++ dog_class.py
@@ -16,6 +16,12 @@ class Dog():
 
 
 my_dog = Dog('willie', 6)
+your_dog = Dog('lucy', 3)
 
 print("My dog's name is " + my_dog.name.title() + ".")
 print("My dog is " + str(my_dog.age) + " years old.")
+my_dog.sit()
+
+print("\nYour dog's name is " + your_dog.name.title() + ".")
+print("Your dog is " + str(your_dog.age) + " year old.")
+your_dog.sit()
