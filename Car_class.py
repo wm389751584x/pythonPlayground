873f2c72333fc6ab6110123568ce0992030e155b
Jason Wang <wangmenghw@gmail.com>
2017-12-10 22:58:54 -0600

    class inheritance in python

--- Car_class.py
+++ Car_class.py
@@ -18,8 +18,32 @@ class Car():
         """Print a statement showing the car's mildage."""
         print("This car has " + str(self.odometer_reading) + " miles on it.")
 
+    def update_oldmeter(self, mileage):
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it attempts to roll the odometer back.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
+
 
 my_new_car = Car('bmw', 'm3', 2018)
 print(my_new_car.get_descriptive_name())
-my_new_car.odometer_reading = 1000
+my_new_car.update_oldmeter(23)
 my_new_car.read_odometer()
+
+print("\n")
+
+my_used_car = Car('honda', 'civic', 2015)
+print(my_used_car.get_descriptive_name())
+my_used_car.update_oldmeter(23500)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
